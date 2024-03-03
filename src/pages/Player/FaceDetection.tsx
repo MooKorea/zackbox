41353c4654a9d1e335171c2f31802a8695b21a38
MooKorea <69235304+MooKorea@users.noreply.github.com>
2024-03-03 17:04:42 -0600

    add face graphic

--- src/pages/Player/FaceDetection.tsx
+++ src/pages/Player/FaceDetection.tsx
@@ -1,98 +1,47 @@
-import * as faceapi from "face-api.js";
-import { useRef, useEffect, useState } from "react";
+import { useRef } from "react";
+import useFaceDetection from "./useFaceDetection";
+import FaceGraphic from "./FaceGraphic";
 
 export default function FaceDetection() {
   const videoRef = useRef<HTMLVideoElement>(null!);
   const canvasRef = useRef<HTMLCanvasElement>(null!);
-  const [isInFrame, setIsInFrame] = useState(false);
-  const [isAreaFit, setIsAreaFit] = useState(false);
 
-  function handleFaceInRange(
-    width: number,
-    height: number,
-    tl: faceapi.Point,
-    tr: faceapi.Point,
-    bl: faceapi.Point,
-    br: faceapi.Point
-  ) {
-    setIsAreaFit(tr.x / width - tl.x / width > 0.4)
-    const midX = (width * 0.5) / width;
-    const midY = (height * 0.5) / height;
-    
-    const gapRadius = 0.1
-    const tlCheck = tl.x / width < midX - gapRadius && tl.y / height < midY - gapRadius;
-    const trCheck = tr.x / width > midX + gapRadius;
-    const blCheck = bl.y / height > midY + gapRadius;
-    setIsInFrame(tlCheck && trCheck && blCheck);
-  }
-
-  useEffect(() => {
-    window.CancelFaceTrack = true;
-    const video = videoRef.current;
-    const canvas = canvasRef.current;
-
-    async function handleFaceTrack() {
-      if (!window.CancelFaceTrack) return;
-      const detections = await faceapi.detectSingleFace(
-        video,
-        new faceapi.TinyFaceDetectorOptions()
-      );
-
-      if (detections !== undefined) {
-        const displaySize = { width: video.clientWidth, height: video.clientHeight };
-        faceapi.matchDimensions(canvas, displaySize);
-        const resizedDetections = faceapi.resizeResults(detections, displaySize);
-        handleFaceInRange(
-          video.clientWidth,
-          video.clientHeight,
-          resizedDetections.box.topLeft,
-          resizedDetections.box.topRight,
-          resizedDetections.box.bottomLeft,
-          resizedDetections.box.bottomRight
-        );
-
-        canvas.getContext("2d")?.clearRect(0, 0, canvas.width, canvas.height);
-        faceapi.draw.drawDetections(canvas, resizedDetections);
-      }
-
-      setTimeout(() => {
-        handleFaceTrack();
-      }, 100);
-    }
-
-    (async () => {
-      if (!navigator.mediaDevices.getUserMedia) return;
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-      video.srcObject = stream;
-      video.addEventListener("loadedmetadata", async () => {
-        video.play();
-        await faceapi.nets.tinyFaceDetector.loadFromUri("./Face_Track");
-        handleFaceTrack();
-      });
-    })();
-  }, []);
+  const { isInFrame, isAreaFit, dimensions } = useFaceDetection(videoRef, canvasRef);
 
   return (
     <>
       <div>{isInFrame ? "face centered" : "face not centered"}</div>
       <div>{isAreaFit ? "face big enough" : "face not big enough"}</div>
-
-      <div className="relative w-screen px-12">
-        <video ref={videoRef} style={{ transform: "rotateY(180deg)" }}></video>
+      <div className="relative w-screen px-12 flex justify-center">
+        <video
+          ref={videoRef}
+          className="brightness-[0.2]"
+          style={{ transform: "rotateY(180deg)" }}
+        ></video>
         <canvas
           ref={canvasRef}
           className="absolute top-0"
           style={{ transform: "rotateY(180deg)" }}
         ></canvas>
-        <button
-          id="stop-interval-button"
-          onClick={() => {
-            window.CancelFaceTrack = false;
-          }}
+        <div className="absolute top-[50%] -translate-y-1/2 w-[15rem] h-[19rem] bg-[#c4c4c4] rounded-full mix-blend-color-dodge"></div>
+        <div
+          className="h-full absolute flex justify-center"
+          style={{ width: dimensions.x }}
         >
-          Stop
-        </button>
+        </div>
+        <FaceGraphic
+          className="absolute top-[50%] -translate-y-1/2 scale-[1.3]"
+          isValid={isAreaFit && isInFrame}
+        />
       </div>
+      <button
+        id="stop-interval-button"
+        onClick={() => {
+          window.CancelFaceTrack = false;
+        }}
+      >
+        Stop
+      </button>
     </>
   );
 }
---
+++ src/pages/Player/useFaceDetection.tsx
@@ -0,0 +1,80 @@
+import * as faceapi from "face-api.js";
+import { useEffect, useState } from "react";
+
+export default function useFaceDetection(
+    videoRef: React.MutableRefObject<HTMLVideoElement>,
+    canvasRef: React.MutableRefObject<HTMLCanvasElement>
+  ) {
+    const [isInFrame, setIsInFrame] = useState(false);
+    const [isAreaFit, setIsAreaFit] = useState(false);
+    type Dimensions = { x: number; y: number };
+    const [dimensions, setDimensions] = useState<Dimensions>({ x: 0, y: 0 });
+  
+    function handleFaceInRange(
+      width: number,
+      height: number,
+      tl: faceapi.Point,
+      tr: faceapi.Point,
+      bl: faceapi.Point,
+      br: faceapi.Point
+    ) {
+      setIsAreaFit(tr.x / width - tl.x / width > 0.4);
+      const midX = (width * 0.5) / width;
+      const midY = (height * 0.5) / height;
+  
+      const gapRadius = 0.1;
+      const tlCheck = tl.x / width < midX - gapRadius && tl.y / height < midY - gapRadius;
+      const trCheck = tr.x / width > midX + gapRadius;
+      const blCheck = bl.y / height > midY + gapRadius;
+      setIsInFrame(tlCheck && trCheck && blCheck);
+    }
+  
+    useEffect(() => {
+      window.CancelFaceTrack = true;
+      const video = videoRef.current;
+      const canvas = canvasRef.current;
+  
+      async function handleFaceTrack() {
+        if (!window.CancelFaceTrack) return;
+        const detections = await faceapi.detectSingleFace(
+          video,
+          new faceapi.TinyFaceDetectorOptions()
+        );
+  
+        if (detections !== undefined) {
+          const displaySize = { width: video.clientWidth, height: video.clientHeight };
+          faceapi.matchDimensions(canvas, displaySize);
+          const resizedDetections = faceapi.resizeResults(detections, displaySize);
+          handleFaceInRange(
+            video.clientWidth,
+            video.clientHeight,
+            resizedDetections.box.topLeft,
+            resizedDetections.box.topRight,
+            resizedDetections.box.bottomLeft,
+            resizedDetections.box.bottomRight
+          );
+  
+          canvas.getContext("2d")?.clearRect(0, 0, canvas.width, canvas.height);
+          faceapi.draw.drawDetections(canvas, resizedDetections);
+        }
+  
+        setTimeout(() => {
+          handleFaceTrack();
+        }, 100);
+      }
+  
+      (async () => {
+        if (!navigator.mediaDevices.getUserMedia) return;
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        video.srcObject = stream;
+        video.addEventListener("loadedmetadata", async () => {
+          video.play();
+          await faceapi.nets.tinyFaceDetector.loadFromUri("./Face_Track");
+          handleFaceTrack();
+          setDimensions({ x: video.clientWidth, y: video.clientHeight });
+        });
+      })();
+    }, []);
+  
+    return { isInFrame, isAreaFit, dimensions };
+  }
